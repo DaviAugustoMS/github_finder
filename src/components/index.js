0cb5f9418057649a01f5a7b8503ddebc72b0071f
DaviAugustoMS <daviaugusto.developer@gmail.com>
2021-12-02 15:02:43 -0300

    feature: create index components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from "./layout/Navbar.js";
+export * from "./layout/Spinner.js";
+export * from "./users/Users.js";
+export * from "./users/UserItem.js";
+export * from "./users/Search.js";

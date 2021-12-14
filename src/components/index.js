0648059480afa56b65557b6ce81c4a5f5d2d6dc7
DaviAugustoMS <daviaugusto.developer@gmail.com>
2021-12-14 20:06:45 -0300

    feat: index components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 export * from "./layout/Navbar.js";
 export * from "./layout/Spinner.js";
+export * from "./layout/Alert.js";
 export * from "./users/Users.js";
+export * from "./users/User.js";
 export * from "./users/UserItem.js";
 export * from "./users/Search.js";
